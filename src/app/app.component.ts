e124721d1c25ce47cface1ef4faea2c31a773af7
Pablo Durán <pabloduran13@gmail.com>
2021-06-05 10:35:40 +0200

    Init Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import Pusher from 'pusher-js';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ng-chat';
+export class AppComponent implements OnInit {
+    title = 'ng-chat';
+
+    username: string = 'username';
+    message: string = '';
+    messages: any[] = [];
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        Pusher.logToConsole = true;
+        const pusher = new Pusher('e709e4a76036b113e861', {
+            cluster: 'eu',
+        });
+
+        const channel = pusher.subscribe('chat');
+        channel.bind('message', (data: any) => {
+            this.messages.push(data);
+        });
+    }
+
+    submit(): void {
+        this.http
+            .post('http://localhost:3000/api/messages', {
+                username: this.username,
+                message: this.message,
+            })
+            .subscribe(() => {
+                this.message = '';
+            });
+    }
 }
